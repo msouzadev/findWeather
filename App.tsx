84a2180139f5c01da5960822dc4e4af33ed282be
Matheus Souza <msouzadev@Matheuss-iMac.local>
2023-03-18 16:04:10 -0300

    setup fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  Overpass_700Bold,
+  Overpass_600SemiBold,
+  Overpass_400Regular,
+  Overpass_300Light,
+  useFonts,
+} from "@expo-google-fonts/overpass";
+import AppLoading from "expo-app-loading";
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Overpass_700Bold,
+    Overpass_600SemiBold,
+    Overpass_400Regular,
+    Overpass_300Light,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
